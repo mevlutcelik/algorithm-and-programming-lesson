f01b53706240825b517e0bf883f2ba788ec0e33b
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 10:53:55 +0300

    Create uygulama6.py

---
+++ Ders 1/uygulama6.py
@@ -0,0 +1,13 @@
+# Dandik bir bilgisayarın fiyatı 10400TL'dir. KDV li fiyatını hesaplayınız
+# IPhone telefonun fiyatı 57000TL'dir. KDV'li fiyatını hesaplayınız
+# KDV'i 0.18 (%18 alınız)
+pc = 10400
+iphone = 57000
+
+print("Telefonun KDV'li Fiyatı:\t")
+print(pc * (pc * 0.18))
+
+print("###############")
+
+print("IPhone'in KDV'li Fiyatı:\t")
+print(iphone * (iphone * 0.18))
