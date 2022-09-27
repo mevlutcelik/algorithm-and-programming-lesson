fc32c4e316091f5ef320dec76747d12e4bdf0479
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 09:57:46 +0300

    Create uygulama3.py

---
+++ Ders 1/uygulama3.py
@@ -0,0 +1,3 @@
+# Soru: 1010 1010 sayısının 10 luk tabandaki karşılığını bulan programı yazınız
+
+print((0 * 2**0) + (1 * 2**1) + (0 * 2**2) + (1 * 2**3) + (0 * 2**4) + (1 * 2**5) + (0 * 2**6) + (1 * 2**7))
