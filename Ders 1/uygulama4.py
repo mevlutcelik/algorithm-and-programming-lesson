9f10d5c0427285356e94a5baf6fdf6455493e393
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 10:12:50 +0300

    Create uygulama4.py

---
+++ Ders 1/uygulama4.py
@@ -0,0 +1,14 @@
+# Bu uygulamada Aritmetik Operatörleri Öğreneceğiz
+
+print(2+2)
+print(1-3)
+print(3.0*5)
+print(9/4)
+print(9//4)
+print(-9/4)
+print(-9//4)
+print(3**5)
+print(3**-5)
+print(123%2)
+print(-123%2)
+print(18%4)
