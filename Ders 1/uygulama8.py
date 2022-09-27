76f10f4c12a193eb8135236ee3ef903ea82b4fd2
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 11:05:58 +0300

    Create uygulama8.py

---
+++ Ders 1/uygulama8.py
@@ -0,0 +1,12 @@
+# Yarı çapı verilen dairenin alanını ve çevresini hesaplayan program yazınız.
+
+r = 8.2
+pi = 3.14
+
+print("Çevre:")
+print(pi * (r ** 2))
+
+print("####################")
+
+print("Alan:")
+print(2 * pi * r)
