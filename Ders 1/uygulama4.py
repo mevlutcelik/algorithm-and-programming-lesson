294d584255aca0982c8098bad0eef14275e709cd
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 10:15:08 +0300

    Update uygulama4.py

--- Ders 1/uygulama4.py
+++ Ders 1/uygulama4.py
@@ -1,14 +1,14 @@
 # Bu uygulamada Aritmetik Operatörleri Öğreneceğiz
 
-print(2+2)
-print(1-3)
-print(3.0*5)
-print(9/4)
-print(9//4)
-print(-9/4)
-print(-9//4)
-print(3**5)
-print(3**-5)
-print(123%2)
-print(-123%2)
-print(18%4)
+print(2+2)                  # Toplama Operatörü
+print(1-3)                  # Çıkarma Operatörü
+print(3.0*5)                # Çarpma Operatörü
+print(9/4)                  # Bölme Operatörü
+print(9//4)                 # Tam Bölme Operatörü
+print(-9/4)                 # Bölme Operatörü
+print(-9//4)                # Tam Bölme Operatörü
+print(3**5)                 # Üs Alma Operatörü
+print(3**-5)                # Üs Alma Operatörü
+print(123%2)                # Mod Alma Operatörü
+print(-123%2)               # Mod Alma Operatörü
+print(18%4)                 # Mod Alma Operatörü
