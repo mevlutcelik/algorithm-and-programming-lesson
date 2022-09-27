7c3fb7b9cd54f4eba802bf454d6749c36f8ab0a3
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-27 10:36:53 +0300

    Create uygulama5.py

---
+++ Ders 1/uygulama5.py
@@ -0,0 +1,11 @@
+# Bu Uygulamada Değişken (Variable) Tanımlamayı göreceğiz
+
+# Değişken tanımlama kuralları
+# 1- Rakam ile başlayamaz
+# 2- Bir değer atamak zorundadır
+# 3- Aynı Değişkene birden fazla değer atanırsa son değer geçerlidir
+# 4- Büyük küçük harf duyarlılığı vardır
+# 5- Özel karakter kullanılamaz -> Türkçe Karakter de olmaz (ç,ş,İ,ğ,ü...)
+
+a = "Deneme"
+print(a)
