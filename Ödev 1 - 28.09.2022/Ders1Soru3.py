7ec3dd760cd2ecd3ed4ced93377f5ce4be0d4a77
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-28 22:20:38 +0300

    Create Ders1Soru3.py

---
+++ Ödev 1 - 28.09.2022/Ders1Soru3.py
@@ -0,0 +1,26 @@
+# Mevlüt Çelik - 28.09.2022
+# Operatörlerü kullanarak ekrana işlem çıktılarını yazdım
+
+
+# Toplama
+print(1 + 3)
+
+
+# Çıkarma
+print(4 - 2)
+
+
+# Çarpma
+print(8 * 16)
+
+
+# Bölme
+print(18 / 3)
+
+
+# Üs Alma
+print(2 ** 3)
+
+
+# Tam Sayılı Bölme
+print(16 // 5)
