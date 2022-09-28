a251f3cbf1cb715f1f62ca0e22fbf7e9c4810eb5
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-28 22:20:42 +0300

    Create Ders1Soru4.py

---
+++ Ödev 1 - 28.09.2022/Ders1Soru4.py
@@ -0,0 +1,8 @@
+# Mevlüt Çelik - 28.09.2022
+# Bu programda değişken tanımlayarak ekrana adımı ve soyadımı yazdırdım
+
+kullaniciAdi = "Mevlüt"
+kullaniciSoyadi = "Çelik"
+
+print("Adınız: " + kullaniciAdi)
+print("Soyadınız: " + kullaniciSoyadi)
