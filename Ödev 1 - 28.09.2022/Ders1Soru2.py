bfd7ff81f82f8ca505f3370d1ff5b10e785438ff
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-28 22:20:34 +0300

    Create Ders1Soru2.py

---
+++ Ödev 1 - 28.09.2022/Ders1Soru2.py
@@ -0,0 +1,27 @@
+# Mevlüt Çelik - 28.09.2022
+# Integer ve Float nedir? Bu terimleri tanımladım
+
+# Integer Sayıları
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+
+
+# Float Sayıları
+print(1.1)
+print(2.2)
+print(3.3)
+print(4.4)
+print(5.5)
+
+
+# INTEGER NEDİR?
+# Tamsayı değer içeren bir veri türüdür.
+# Integer değer olarak -2.147.483.648 ile 2.147.483.647 arasında değişen imzalı 32 bitlik (4 bayt) tamsayıları tutar.
+
+
+#FLOAT NEDİR?
+# Büyük ve ondalıklı sayılar tanımlanabilen veri türüdür. 
+# 32 Bit boyutundadır ve 1.4×10^-45 ile 3.4×10^38 aralığında bir değer tanımlanabilir. Float içerisine tam sayı yazdığımız zamanda bile o sayı 1.0 şeklinde ondalıklı olarak algılar.
