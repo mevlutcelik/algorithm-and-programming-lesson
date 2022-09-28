de6d3883762bbad25e62ae3f11fcaed1e1b7c4da
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-28 22:20:52 +0300

    Create Ders1Soru6.py

---
+++ Ödev 1 - 28.09.2022/Ders1Soru6.py
@@ -0,0 +1,9 @@
+# Mevlüt Çelik - 28.09.2022
+# Vize Final Hesaplama uygulaması
+
+
+vize = float(input("Vize notunuzu giriniz...:"))
+final = float(input("Final notunuzu giriniz...:"))
+
+
+ortalama = print("Ortalamanız: " + str((vize * 0.4) + (final * 0.6)))
