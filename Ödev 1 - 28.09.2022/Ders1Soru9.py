ee497092c2ca6a6c610e578a2809d3827e9cef8c
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-29 20:36:06 +0300

    Create Ders1Soru9.py

---
+++ Ödev 1 - 28.09.2022/Ders1Soru9.py
@@ -0,0 +1,7 @@
+# Mevlüt Çelik - 29.09.2022
+# Bu dosyaya neden girilen bir sayı değerinin işlem yapılabilmesi için int veya float a çevrilmesi gerektiğini detaylı bir şekilde yazdım.
+
+
+# Girilen sayı değeri eğer tam sayı olacaksa int (integer) veri tipinde alınmalıdır.
+# Alınan sayı değeri tam sayı (int, integer) ise eğer aritmetik operatör işlemi sonucu çıkan sonuç ondalıklı sayı ise float veri tipine dönüştürülmelidir.
+# Alınan sayı değeri ondalıklı sayı (float) ise eğer aritmetik operatör işlemi sonucu çıkan sonuç tam sayı ise int (integer) veri tipine dönüştürülmelidir.
