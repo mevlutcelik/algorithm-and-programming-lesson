aeef37a231241f280b11283e1cfa5b125f21cb7b
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:53:22 +0300

    Create uygulama9.py

---
+++ Ödev 3 - 07.10.2022/uygulama9.py
@@ -0,0 +1,31 @@
+# Mevlüt Çelik
+# 07.10.2022
+# type komutu python da ne işe yaramaktadır internetten araştırıp tüm veri tiplerinden vereceğiniz örnekle açıklayınız.
+
+
+# type() metodu veri tipinin hangi türde olduğunu bizlere söyler.
+
+
+# String: Karakter Dizisi
+print(type("Deneme"))
+# Ekran çıktısı: <class 'str'>
+
+
+# Int (Integer): Tam Sayı
+print(type(23))
+# Ekran çıktısı: <class 'int'>
+
+
+# Float: Ondalıklı Sayı
+print(type(23.0))
+# Ekran çıktısı: <class 'float'>
+
+
+# Boolean: Mantıksal Veri Tipi
+print(type(True))
+# Ekran çıktısı: <class 'bool'>
+
+
+# List: Dizi Veri Tipi
+print(type([1,2,3,4,5,6]))
+# Ekran çıktısı: <class 'list'>
