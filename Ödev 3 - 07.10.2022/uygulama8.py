57234b16a99afef0f97c8320dc860cc52a8c18c3
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:43:29 +0300

    Update uygulama7.py

---
+++ Ödev 3 - 07.10.2022/uygulama8.py
@@ -0,0 +1,11 @@
+# Mevlüt Çelik
+# 07.10.2022
+# ASCII tabloya göre klavyeden basılan 2 tuşunun 16 lık tabandaki karşılığı nedir?
+
+
+# Klavye'den basılan 2 tuşunun karşılıkları:
+# Decimal (10 luk taban) = 50
+# Hexadecimal (16 lık taban) = 32
+# Binary (2 lik taban) = 1100110
+# Octal (8 lik taban) = 62
+# Char (Karakter, Klavyede basılan tuş) = 2
