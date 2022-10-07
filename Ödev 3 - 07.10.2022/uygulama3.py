f4566402528b20f91d64229ffb0215e9fb6d6b6e
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:15:54 +0300

    Create uygulama3.py

---
+++ Ödev 3 - 07.10.2022/uygulama3.py
@@ -0,0 +1,11 @@
+# Mevlüt Çelik
+# 07.10.2022
+# Pythonda aritmetik operatörler nelerdir yazınız. 
+
+# Toplama (+)
+# Çıkarma (-)
+# Çarpma (*)
+# Üs Alma (**)
+# Bölme (/)
+# Tam Bölme (//)
+# Mod Alma (%)
