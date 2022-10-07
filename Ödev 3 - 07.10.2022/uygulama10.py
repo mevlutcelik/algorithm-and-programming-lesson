b9ac6fcfd9003d1c7e3a65dec075d0b406724aa0
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:58:19 +0300

    Create uygulama10.py

---
+++ Ödev 3 - 07.10.2022/uygulama10.py
@@ -0,0 +1,11 @@
+# Mevlüt Çelik
+# 07.10.2022
+# type komutu python da ne işe yaramaktadır internetten araştırıp tüm veri tiplerinden vereceğiniz örnekle açıklayınız.
+
+
+# id() metodu o nesnenin kimliğini döndürür. Her nesneye (değişkene, objeye, classa) ait uniq bir kimlik değeri döner.
+
+
+dizi = id(('Türkçe', 'Matematik', 'Fizik'))
+
+print(dizi)
