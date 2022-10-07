f39ee84c21e0365afc09e1c943c7ef1795d87183
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:10:32 +0300

    Create uygulama2.py

---
+++ Ödev 3 - 07.10.2022/uygulama2.py
@@ -0,0 +1,10 @@
+# Mevlüt Çelik
+# 07.10.2022
+# Python da veri tipleri nelerdir yazınız.
+
+# String: Karakter Dizisi
+# Integer: Tam Sayı
+# Float: Ondalıklı Sayı
+# Boolean: Mantıksal Veri Tipi
+# List: Dizi Veri Tipi
+# Bytes: Binary Veri Tipi
