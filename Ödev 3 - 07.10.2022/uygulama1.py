f4dd4c46e30b307ed28fe7a5bf4442e29a4d8648
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:06:22 +0300

    Create uygulama1.py

---
+++ Ödev 3 - 07.10.2022/uygulama1.py
@@ -0,0 +1,14 @@
+# Mevlüt Çelik
+# 07.10.2022
+# Adınız soyadınız, tarih, dersin kodu verilerini değişkene aktararak ekrana yazdırınız.
+
+
+adsoyad = "Mevlüt Çelik"
+tarih = "07.10.2022"
+kod = "BST 103"
+
+
+# Ekrana yazdıralım
+print("Ad Soyad: " + adsoyad)
+print("Tarih: " + tarih)
+print("Dersin Kodu: " + kod)
