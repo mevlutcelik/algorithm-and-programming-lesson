3e94a75b66ab55c80bed109bb9666ee31f25fa6b
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:21:11 +0300

    Create uygulama4.py

---
+++ Ödev 3 - 07.10.2022/uygulama4.py
@@ -0,0 +1,7 @@
+# Mevlüt Çelik
+# 07.10.2022
+# 1011 1101 sayısının 10 luk tabanda karşılığını hesaplayan programı yazınız.
+
+soru = "1011 1101 sayısının 10 luk tabandaki karşılığı: "
+islem = str((2 ** 0) + (2 ** 2) + (2 ** 3) + (2 ** 4) + (2 ** 5) + (2 ** 7))
+print(soru + islem)
