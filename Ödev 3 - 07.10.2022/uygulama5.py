b3512d77ea6083364549332f268ba35d4e90f630
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-07 15:24:11 +0300

    Create uygulama5.py

---
+++ Ödev 3 - 07.10.2022/uygulama5.py
@@ -0,0 +1,12 @@
+# Mevlüt Çelik
+# 07.10.2022
+# Kullanıcıdan üçgenin kenar uzunluklarını alarak çevresini hesaplayan ve sonucu ekrana yazdıran programı yazınız.
+
+
+kenar1 = float(input("Üçgenin 1. kenar uzunluğunu giriniz: "))
+kenar2 = float(input("Üçgenin 2. kenar uzunluğunu giriniz: "))
+kenar3 = float(input("Üçgenin 3. kenar uzunluğunu giriniz: "))
+cevre = str(kenar1 + kenar2 + kenar3)
+
+
+print("Üçgenin çevresinin uzunluğu: " + cevre)
