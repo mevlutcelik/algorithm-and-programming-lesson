ced843e1bd79ad2b33a021d45f0a0b61840fac68
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-11 10:07:42 +0300

    Create uygulama13.py

---
+++ Ders 4/uygulama13.py
@@ -0,0 +1,12 @@
+websitesi = "https://www.btkakademi.gov.tr"
+ders = "Yazukun Hareketi kapsamında sadece size özel açılmış olan ders."
+
+print(f"Ders karakter dizisinde {len(ders)} karakter bulunmaktadır.")
+
+print(websitesi[8:11])
+
+print(websitesi[-6:-3])
+
+print(ders[15: -15])
+
+print(ders[::-1])
