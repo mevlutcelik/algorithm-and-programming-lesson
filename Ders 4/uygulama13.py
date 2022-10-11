10a6d36adc131ba1406b9a21ab8209a580ec1dd6
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-11 10:12:21 +0300

    Update uygulama13.py

--- Ders 4/uygulama13.py
+++ Ders 4/uygulama13.py
@@ -1,12 +1,17 @@
 websitesi = "https://www.btkakademi.gov.tr"
-ders = "Yazukun Hareketi kapsamında sadece size özel açılmış olan ders."
+ders = "Yazılım Hareketi kapsamında sadece size özel açılmış olan ders."
 
+# Cevap 1
 print(f"Ders karakter dizisinde {len(ders)} karakter bulunmaktadır.")
 
+# Cevap 2
 print(websitesi[8:11])
 
+# Cevap 3
 print(websitesi[-6:-3])
 
+# Cevap 4
 print(ders[15: -15])
 
+# Cevap 5
 print(ders[::-1])
