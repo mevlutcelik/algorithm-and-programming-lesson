b2922840d41177fb0f447504f4086cf6242ba2ac
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-11 09:41:26 +0300

    Update uygulama10.py

--- Ders 4/uygulama10.py
+++ Ders 4/uygulama10.py
@@ -3,6 +3,7 @@ soyad = "Çelik"
 yas = 19
 
 print("Benim adım {ad} soyadım {soyad} ve ben {yas} yaşındayım".format(ad = ad, soyad = soyad, yas = yas))
+
 deneme = """
 Denenaskldsjadl kjaskldjasklja klajf kljfklafj klafj akls işişsadiasfialğpasofğafkşla
 asdsadsadsadsadsad
