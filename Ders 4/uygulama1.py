8ca8cdc8c2b62cd8150b19cf495439f099870a4b
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-10-11 09:09:40 +0300

    Update uygulama1.py

--- Ders 4/uygulama1.py
+++ Ders 4/uygulama1.py
@@ -2,4 +2,4 @@ ad = "Mevlüt"
 soyad = "Çelik"
 yas = 19
 
-print("Benim adım " + ad + ", soyadım " + soyad + " ve yaşım " + yas)
+print("Benim adım " + ad + ", soyadım " + soyad + " ve yaşım " + str(yas))
