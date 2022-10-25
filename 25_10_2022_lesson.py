888677f6623646f806d9fdfc04f5834e2c6ac454
Mevlüt Çelik <98541209+mevlutcelik@users.noreply.github.com>
2022-10-25 10:18:26 +0300

    Create 25_10_2022_lesson.py

---
+++ 25_10_2022_lesson.py
@@ -0,0 +1,17 @@
+sehirler = ["Mersin", "Muş", "Antalya"]
+plakalar = [33,49,7]
+
+
+print(plakalar[sehirler.index("Mersin")])
+
+
+plakalar = { "key": "value" } # Key: anahtar, Value: değer
+
+
+plakalar = {
+    "Mersin"    : 33,
+    "Muş"       : 49,
+    "Antalya"   : 7
+}
+
+print(plakalar)
